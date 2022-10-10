64567aa4e53a084f493a22ab6500371dbf5fe7c0
ximiximi1 <84178026+ximiximi1@users.noreply.github.com>
2022-10-10 16:56:02 +0800

    Update qbittorrent-nox-static.sh

--- qbittorrent-nox-static.sh
+++ qbittorrent-nox-static.sh
@@ -1061,7 +1061,7 @@ _multi_arch() {
 			mkdir -p "${qbt_install_dir}/logs"
 
 			if [[ "${qbt_cross_target}" =~ ^(alpine)$ && ! -f "${qbt_install_dir}/${qbt_cross_host}.tar.gz" ]]; then
-				curl "https://github.com/userdocs/qbt-musl-cross-make/releases/latest/download/${qbt_cross_host}.tar.gz" > "${qbt_install_dir}/${qbt_cross_host}.tar.gz"
+				curl "https://github.com/ximiximi1/qbt-musl-cross-make/releases/latest/download/${qbt_cross_host}.tar.gz" > "${qbt_install_dir}/${qbt_cross_host}.tar.gz"
 				tar xf "${qbt_install_dir}/${qbt_cross_host}.tar.gz" --strip-components=1 -C "${qbt_install_dir}"
 			fi
 
